bc3ee1a880933f7d056308b61886ca31ff82f42a
ice <ice@fxhome.local>
2013-11-24 14:17:55 +0900

    fix output filter

--- src/Network/AD/SID.hs
+++ src/Network/AD/SID.hs
@@ -3,6 +3,7 @@ module Network.AD.SID where
 
 import System.Process
 import System.Exit
+import Data.Char
 
 type SIDT = String
 
@@ -16,16 +17,20 @@ type SIDs = [SID]
 getCurrentUserSID:: IO (Either String SID)
 getCurrentUserSID = do
     (!idcode, !idout, !iderr) <- readProcessWithExitCode "id" ["-u"] ""
+    putStrLn $! idout
+    putStrLn $! iderr
+    let uid = filter isDigit idout
     if idcode /= ExitSuccess then return $! Left $! "id -u returned " ++ 
             show idcode
         else do
             (!wbcode, !wbout, !wberr) <- readProcessWithExitCode "wbinfo" 
-                ["--uid-to-sid " ++ idout] ""
+                ["--uid-to-sid=" ++ uid] ""
             putStrLn $! wbout
             putStrLn $! wberr
             if wbcode /= ExitSuccess
                 then return $! Left $! "wbinfo returned " ++ show wbcode
-                else return $! Right $! SIDUser wbout
+                else return $! Right $! SIDUser $!
+                    filter (\x-> isDigit x || isAlpha x || x=='-') wbout
     
 
 getCurrentGroupsSIDs:: IO [SID]
