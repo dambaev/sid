87a3ddb96546f4e880fb6b9a57b7bdd49e41249c
ice <ice@fxhome.local>
2013-11-24 14:36:03 +0900

    getCurrentGroupsSIDs

--- src/Network/AD/SID.hs
+++ src/Network/AD/SID.hs
@@ -4,6 +4,8 @@ module Network.AD.SID where
 import System.Process
 import System.Exit
 import Data.Char
+import Data.List.Utils
+import Control.Concurrent
 
 type SIDT = String
 
@@ -17,21 +19,24 @@ type SIDs = [SID]
 getCurrentUserSID:: IO (Either String SID)
 getCurrentUserSID = do
     (!idcode, !idout, !iderr) <- readProcessWithExitCode "id" ["-u"] ""
-    putStrLn $! idout
-    putStrLn $! iderr
     let uid = filter isDigit idout
     if idcode /= ExitSuccess then return $! Left $! "id -u returned " ++ 
-            show idcode
+            iderr
         else do
             (!wbcode, !wbout, !wberr) <- readProcessWithExitCode "wbinfo" 
                 ["--uid-to-sid=" ++ uid] ""
-            putStrLn $! wbout
-            putStrLn $! wberr
             if wbcode /= ExitSuccess
-                then return $! Left $! "wbinfo returned " ++ show wbcode
+                then return $! Left $! "wbinfo returned " ++ wberr
                 else return $! Right $! SIDUser $!
                     filter (\x-> isDigit x || isAlpha x || x=='-') wbout
     
 
-getCurrentGroupsSIDs:: IO [SID]
-getCurrentGroupsSIDs = undefined
+getCurrentGroupsSIDs:: SID-> IO (Either String [SID])
+getCurrentGroupsSIDs (SIDUser sid ) = do
+    (!wbcode, !wbout, !wberr) <- readProcessWithExitCode "wbinfo" 
+        ["--user-sids=" ++ sid] ""
+    if wbcode /= ExitSuccess
+        then return $! Left $! "wbinfo returned: " ++ wberr
+        else do
+            let !lined = lines wbout
+            return $! Right $! map (SIDGroup) lined
