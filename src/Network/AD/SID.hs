9a67209dd6437b7e33f6dca9e19b2dc4580ba469
ice <ice@fxhome.local>
2013-11-24 13:51:48 +0900

    initial

---
+++ src/Network/AD/SID.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE BangPatterns #-}
+module Network.AD.SID where
+
+import System.Process
+import System.Exit
+
+type SIDT = String
+
+data SID = SIDUser SIDT
+         | SIDGroup SIDT
+    deriving (Eq, Show)
+type SIDs = [SID]
+
+
+
+getCurrentUserSID:: IO (Either String SID)
+getCurrentUserSID = do
+    (!idcode, !idout, !iderr) <- readProcessWithExitCode "id" ["-u"] ""
+    if idcode /= ExitSuccess then return $! Left $! "id -u returned " ++ 
+            show idcode
+        else do
+            (!wbcode, !wbout, !wberr) <- readProcessWithExitCode "wbinfo" 
+                ["--uid-to-sid " ++ idout] ""
+            if wbcode /= ExitSuccess
+                then return $! Left $! "wbinfo returned " ++ show wbcode
+                else return $! Right $! SIDUser wbout
+    
+
+getCurrentGroupsSIDs:: IO [SID]
+getCurrentGroupsSIDs = undefined
